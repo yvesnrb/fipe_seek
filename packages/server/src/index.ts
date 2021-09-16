b17c4f5effa4a51635c4b5d20a2e55697bff93ea
Yves Bode <ynrbode@gmail.com>
2021-09-16 11:36:27 -0300

    feat(server): implements a database connection on the entry point

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,20 @@
+/* eslint-disable no-console */
 import 'dotenv/config';
 
 import { server } from '@http/server';
 import { serverConfig } from '@config/server.config';
+import { mongoClient } from '@/db';
+
+mongoClient
+  .connect()
+  .then(() => {
+    console.log('💾 MongoDB connected');
+  })
+  .catch(reason => {
+    console.log('MongoBD connection failed');
+    console.log(reason);
+  });
 
 server.listen(serverConfig.port, () => {
-  // eslint-disable-next-line no-console
   console.log(`🚀 Server listening on localhost:${serverConfig.port}`);
 });
