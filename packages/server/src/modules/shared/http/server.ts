bb01bb2dacb2822ebc9b3d7b9d73dd3a7ef5a409
Yves Bode <ynrbode@gmail.com>
2021-09-16 17:24:30 -0300

    feat(server): implements static file serving from the 'public' folder

--- packages/server/src/modules/shared/http/server.ts
+++ packages/server/src/modules/shared/http/server.ts
@@ -1,6 +1,7 @@
 import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 // import { errors } from 'celebrate';
 
 import { handleError } from '@http/middleware/handle-error.middleware';
@@ -11,6 +12,7 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 server.use(indexRouter);
+server.use(express.static(path.resolve(__dirname, '..', '..', '..', 'public')));
 
 // server.use(errors());
 server.use(handleError);
