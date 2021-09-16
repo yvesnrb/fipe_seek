b950ae7c069cc6d0a99a7bb3e0f9d6b998a51e90
Yves Bode <ynrbode@gmail.com>
2021-09-16 13:37:51 -0300

    feat(server): implements the 'list' method on the 'BrandsController' class

--- packages/server/src/modules/fipe/http/controllers/brands.controller.ts
+++ packages/server/src/modules/fipe/http/controllers/brands.controller.ts
@@ -1,7 +1,21 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { typeType } from '@providers/i-fipe.provider';
+import { ListBrandsService } from '@services/list-brands.service';
 
 export class BrandsController {
-  public async list(_request: Request, response: Response): Promise<void> {
-    response.json({ status: 'ok' });
+  private listBrands: ListBrandsService;
+
+  constructor() {
+    this.listBrands = container.resolve(ListBrandsService);
+  }
+
+  public async list(request: Request, response: Response): Promise<void> {
+    const { type } = request.params;
+
+    const list = await this.listBrands.execute({ type: type as typeType });
+
+    response.json(list);
   }
 }
