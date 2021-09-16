c5a0101308f5e6e4d0ed581c9f69b45322f8e885
Yves Bode <ynrbode@gmail.com>
2021-09-16 13:36:39 -0300

    feat(server): adds the 'FipeProvider' injection token to the fipe container
    This token will resolve to a singleton of 'BrasilApiFipeProvider'.

--- packages/server/src/modules/fipe/containers/fipe.container.ts
+++ packages/server/src/modules/fipe/containers/fipe.container.ts
@@ -3,9 +3,12 @@ import { container } from 'tsyringe';
 import { mongoClient } from '@/db';
 import { mongoConfig } from '@config/mongo.config';
 import { Vehicle } from '@entities/vehicle.entity';
+import { BrasilApiFipeProvider } from '@providers/brasil-api-fipe.provider';
 
 container.register('VehiclesCollection', {
   useValue: mongoClient
     .db(mongoConfig.database)
     .collection<Vehicle>('vehicles'),
 });
+
+container.registerSingleton('FipeProvider', BrasilApiFipeProvider);
