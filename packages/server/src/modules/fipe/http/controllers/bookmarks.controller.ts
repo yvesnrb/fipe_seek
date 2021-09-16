f6201c444d08f39f49a031308a031b8e407e400b
Yves Bode <ynrbode@gmail.com>
2021-09-16 15:58:17 -0300

    feat(server): implements the 'create' method on the 'BookmarksController' class

--- packages/server/src/modules/fipe/http/controllers/bookmarks.controller.ts
+++ packages/server/src/modules/fipe/http/controllers/bookmarks.controller.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateBookmarkService } from '@services/create-bookmark.service';
 
 export class BookmarksController {
+  private createBookmark: CreateBookmarkService;
+
+  constructor() {
+    this.createBookmark = container.resolve(CreateBookmarkService);
+  }
+
   public async list(_request: Request, response: Response): Promise<void> {
     response.json({ status: 'ok' });
   }
 
-  public async create(_request: Request, response: Response): Promise<void> {
-    response.json({ status: 'ok' });
+  public async create(request: Request, response: Response): Promise<void> {
+    const { _id } = request.body;
+    const vehicle = await this.createBookmark.execute({ _id });
+
+    response.json(vehicle);
   }
 }
