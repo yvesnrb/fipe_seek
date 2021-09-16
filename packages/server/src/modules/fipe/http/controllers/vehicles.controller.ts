0c103599f110d4bfaf5942a996d5a4e279af3207
Yves Bode <ynrbode@gmail.com>
2021-09-16 14:56:21 -0300

    feat(server): implements the 'list' method of the 'VehiclesController' class

--- packages/server/src/modules/fipe/http/controllers/vehicles.controller.ts
+++ packages/server/src/modules/fipe/http/controllers/vehicles.controller.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { ListVehiclesService } from '@services/list-vehicles.service';
 
 export class VehiclesController {
-  public async list(_request: Request, response: Response): Promise<void> {
-    response.json({ status: 'ok' });
+  private listVehicles: ListVehiclesService;
+
+  constructor() {
+    this.listVehicles = container.resolve(ListVehiclesService);
+  }
+
+  public async list(request: Request, response: Response): Promise<void> {
+    const { code } = request.params;
+    const list = await this.listVehicles.execute({ code });
+
+    response.json(list);
   }
 }
