7a5fa4cae92035f09ebbfd8aa2b828d27f0ee46c
Yves Bode <ynrbode@gmail.com>
2021-09-16 15:55:32 -0300

    feat(server): adds the 'BookmarksCollection' injection token to the fipe container
    This token should resolve to the value returned from 'mongoClient'.

--- packages/server/src/modules/fipe/containers/fipe.container.ts
+++ packages/server/src/modules/fipe/containers/fipe.container.ts
@@ -3,12 +3,19 @@ import { container } from 'tsyringe';
 import { mongoClient } from '@/db';
 import { mongoConfig } from '@config/mongo.config';
 import { Vehicle } from '@entities/vehicle.entity';
+import { Bookmark } from '@entities/bookmark.entity';
 import { BrasilApiFipeProvider } from '@providers/brasil-api-fipe.provider';
 
 container.register('VehiclesCollection', {
   useValue: mongoClient
     .db(mongoConfig.database)
     .collection<Vehicle>('vehicles'),
+});
+
+container.register('BookmarksCollection', {
+  useValue: mongoClient
+    .db(mongoConfig.database)
+    .collection<Bookmark>('bookmarks'),
 });
 
 container.registerSingleton('FipeProvider', BrasilApiFipeProvider);
