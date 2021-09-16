7dd533ddd67d65e42fca1ae01f1b49520c9c136d
Yves Bode <ynrbode@gmail.com>
2021-09-16 14:55:25 -0300

    feat(server): implements the 'listVehicles' method of the 'BrasilApiFipeProvider' class

--- packages/server/src/modules/fipe/providers/brasil-api-fipe.provider.ts
+++ packages/server/src/modules/fipe/providers/brasil-api-fipe.provider.ts
@@ -1,12 +1,22 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 import { IFipeProvider, typeType } from '@providers/i-fipe.provider';
 import { Brand } from '@entities/brand.entity';
 import { Vehicle } from '@entities/vehicle.entity';
+import { AppError } from '@errors/app.error';
 
 interface IMarca {
   nome: string;
   valor: string;
+}
+
+interface IVeiculo {
+  valor: string;
+  marca: string;
+  modelo: string;
+  anoModelo: number;
+  combustivel: string;
+  codigoFipe: string;
 }
 
 export class BrasilApiFipeProvider implements IFipeProvider {
@@ -31,8 +41,29 @@ export class BrasilApiFipeProvider implements IFipeProvider {
     return list;
   }
 
-  public async listVehicles(_fipeCode: string): Promise<Vehicle[]> {
-    throw new Error('Method not implemented.');
+  public async listVehicles(fipeCode: string): Promise<Vehicle[]> {
+    const response = await axios
+      .get<IVeiculo[]>(`${this.url}/preco/v1/${fipeCode}`)
+      .catch(reason => {
+        const error = reason as AxiosError;
+        if (error.response?.status === 400)
+          throw new AppError('invalid fipeCode vehicle search attempt', 400);
+
+        throw new AppError('unexpected BrasilAPI error', 500);
+      });
+
+    const list: Vehicle[] = response.data.map(veiculo => {
+      return new Vehicle({
+        fipeCode: veiculo.codigoFipe,
+        brand: veiculo.marca,
+        fuel: veiculo.combustivel,
+        year: veiculo.anoModelo,
+        model: veiculo.modelo,
+        price: veiculo.valor,
+      });
+    });
+
+    return list;
   }
 
   public async getVehicle(_id: string): Promise<Vehicle> {
