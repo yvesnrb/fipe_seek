fda497bc2d43692bf59325d69d2581a4f2060386
Yves Bode <ynrbode@gmail.com>
2021-09-16 16:46:35 -0300

    feat(server): implements the 'list' method of the 'BookmarksController' class

--- packages/server/src/modules/fipe/http/controllers/bookmarks.controller.ts
+++ packages/server/src/modules/fipe/http/controllers/bookmarks.controller.ts
@@ -1,17 +1,23 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
+import { ListBookmarksService } from '@services/list-bookmarks.service';
 import { CreateBookmarkService } from '@services/create-bookmark.service';
 
 export class BookmarksController {
+  private listBookmarks: ListBookmarksService;
+
   private createBookmark: CreateBookmarkService;
 
   constructor() {
+    this.listBookmarks = container.resolve(ListBookmarksService);
     this.createBookmark = container.resolve(CreateBookmarkService);
   }
 
   public async list(_request: Request, response: Response): Promise<void> {
-    response.json({ status: 'ok' });
+    const list = await this.listBookmarks.execute();
+
+    response.json(list);
   }
 
   public async create(request: Request, response: Response): Promise<void> {
