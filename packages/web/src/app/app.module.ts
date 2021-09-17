846efff39dabe2399e9e5fad7621a67079e9c461
Yves Bode <ynrbode@gmail.com>
2021-09-16 21:38:41 -0300

    chore(web): scaffolds the 'IndexComponent'

--- packages/web/src/app/app.module.ts
+++ packages/web/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './pages/index/index.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, IndexComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
